e9a2192b007c04cf735696e13788defdccf5eb3c
Ronaldo <dawing.diefouo@gmail.com>
2026-01-22 01:05:08 +0100

    Allow S3 origin in CORS and point frontend to ALB

--- backend/src/main/java/io/werescuecats/backend/config/SecurityConfig.java
+++ backend/src/main/java/io/werescuecats/backend/config/SecurityConfig.java
@@ -54,7 +54,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList(
+                "http://localhost:4200",
+                "http://cloud-semesterprojekt-2026.s3-website-us-east-1.amazonaws.com"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
